f46059bcdb8f7b8535536abfa4e2fada1bb0eb3b
Fiky Baharudin <fikyb13@gmail.com>
2025-01-05 08:53:44 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://efbe404.github.io",
-	base: "/",
+	base: "/dist",
 	integrations: [sitemap()],
 	markdown: {
 		shikiConfig: {
